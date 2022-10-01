cce0c1325fcab1f18faa750cc53263286a05bf5d
ldoig <lachjd@protonmail.com>
2022-10-02 09:20:19 +1100

    Select guess w/ max entropy'

--- Proj2.hs
+++ Proj2.hs
@@ -5,88 +5,95 @@ module Proj2
     GameState,
     initialGuess,
     nextGuess,
-    Chord,
-    Feedback,
-    allChords,
   )
 where
 
-import Data.Char (digitToInt)
-import Data.List (delete, group, maximumBy, sort)
+import Data.List (delete, group, maximumBy, sort, subsequences)
 import Data.Maybe (fromJust)
 import Data.Ord (comparing)
+import Text.Read (readMaybe)
 
-data Pitch = Pitch Char Int
-  deriving (Eq, Ord)
+data Note = A | B | C | D | E | F | G
+  deriving (Eq, Read, Show, Enum, Bounded)
 
-instance Show Pitch where
-  show (Pitch note oct) = [note, head (show oct)]
+data Octave = O1 | O2 | O3
+  deriving (Eq, Read, Show, Enum, Bounded)
 
-type Chord = [Pitch]
+data Pitch = Pitch Note Octave
+  deriving (Eq)
+
+instance Show Pitch where show (Pitch note oct) = show note ++ tail (show oct)
 
 newtype GameState = GameState [Chord]
 
+type Chord = [Pitch]
+
 type Feedback = (Int, Int, Int)
 
-notes = ['A' .. 'G']
+allPitches :: [Pitch]
+allPitches = [Pitch note octave | note <- [minBound ..], octave <- [minBound ..]]
 
-octs = [1 .. 3]
-
-pitches = [Pitch n o | o <- octs, n <- notes]
-
-allChords =
-  [ [pitches !! i, pitches !! j, pitches !! k]
-    | i <- [0 .. length pitches - 1],
-      j <- [i + 1 .. length pitches - 1],
-      k <- [j + 1 .. length pitches - 1]
-  ]
+allChords :: [Chord]
+allChords = filter ((== 3) . length) (subsequences allPitches)
 
 toPitch :: String -> Maybe Pitch
-toPitch [note, octChar]
-  | note `elem` notes && oct `elem` octs = Just (Pitch note oct)
-  | otherwise = Nothing
-  where
-    oct = digitToInt octChar
-toPitch _ = Nothing
-
-toChord :: String -> [Pitch]
-toChord = fromJust . mapM toPitch . words
+toPitch pitchStr =
+  let (noteStr, octaveStr) = splitAt 1 pitchStr
+   in case (readMaybe noteStr, readMaybe ('O' : octaveStr)) of
+        (Just note, Just octave) -> Just (Pitch note octave)
+        _ -> Nothing
 
 feedback :: Chord -> Chord -> Feedback
-feedback target guess = (samePitch, sameNote - samePitch, sameOct - samePitch)
+feedback guess target =
+  (numSamePitch, numSameNote - numSamePitch, numSameOctave - numSamePitch)
   where
-    samePitch = countCommon target guess
-    (targetNotes, targetOcts) = splitChord target
-    (guessNotes, guessOcts) = splitChord guess
-    sameNote = countCommon targetNotes guessNotes
-    sameOct = countCommon targetOcts guessOcts
+    numSamePitch = numCommonElements target guess
+    (targetNotes, targetOctaves) = splitChord target
+    (guessNotes, guessOctaves) = splitChord guess
+    numSameNote = numCommonElements targetNotes guessNotes
+    numSameOctave = numCommonElements targetOctaves guessOctaves
 
-countCommon :: Eq a => [a] -> [a] -> Int
-countCommon [] _ = 0
-countCommon (x : xs) ys
-  | x `elem` ys = 1 + countCommon xs (delete x ys)
-  | otherwise = countCommon xs ys
+numCommonElements :: Eq a => [a] -> [a] -> Int
+numCommonElements [] _ = 0
+numCommonElements (x : xs) ys
+  | x `elem` ys = 1 + numCommonElements xs (delete x ys)
+  | otherwise = numCommonElements xs ys
 
-splitChord :: Chord -> ([Char], [Int])
+splitChord :: Chord -> ([Note], [Octave])
 splitChord [Pitch n1 o1, Pitch n2 o2, Pitch n3 o3] = ([n1, n2, n3], [o1, o2, o3])
 splitChord _ = error "invalid chord"
 
 initialGuess :: (Chord, GameState)
 initialGuess = (guess, GameState (delete guess allChords))
   where
-    guess = toChord "G2 E3 F3"
+    guess = map (fromJust . toPitch) ["E2", "F3", "G3"]
 
 nextGuess :: (Chord, GameState) -> Feedback -> (Chord, GameState)
-nextGuess (guess, GameState targets) yourFeedback =
-  (nextGuess, GameState (delete nextGuess remainingTargets))
+nextGuess (oldGuess, GameState oldTargets) yourFeedback =
+  (newGuess, GameState (delete newGuess newTargets))
   where
-    remainingTargets = filter ((== yourFeedback) . feedback guess) targets
-    scoreOrdering = comparing (guessScore remainingTargets)
-    nextGuess = maximumBy scoreOrdering remainingTargets
+    newTargets = consistentTargets yourFeedback oldGuess oldTargets
+    newGuess = maxEntropyGuess newTargets
 
-guessScore :: [Chord] -> Chord -> Int
-guessScore targets guess = - sumOfSquares (countDistinct potentialFeedbacks)
+consistentTargets :: Feedback -> Chord -> [Chord] -> [Chord]
+consistentTargets yourFeedback guess =
+  filter ((== yourFeedback) . feedback guess)
+
+maxEntropyGuess :: [Chord] -> Chord
+maxEntropyGuess targets = maximumBy (comparing (guessEntropy targets)) targets
+
+guessEntropy :: [Chord] -> Chord -> Double
+guessEntropy targets guess = entropy potentialFeedbacks
   where
     potentialFeedbacks = map (feedback guess) targets
-    countDistinct = map length . group . sort
-    sumOfSquares = sum . map (^ 2)
+
+entropy :: Ord a => [a] -> Double
+entropy xs = sum [- p * log p | p <- normalisedFrequencies xs]
+
+normalisedFrequencies :: Ord a => [a] -> [Double]
+normalisedFrequencies xs = [freq `floatDiv` length xs | freq <- frequencies xs]
+  where
+    floatDiv x y = fromIntegral x / fromIntegral y
+
+frequencies :: Ord a => [a] -> [Int]
+frequencies = map length . group . sort
