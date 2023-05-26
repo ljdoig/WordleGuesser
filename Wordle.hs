eda0ebad774867276b7d690b6f8a4ab05a058383
ldoig <lachjd@protonmail.com>
2023-05-26 15:00:59 +1000

    Implement Wordle feedback

--- Wordle.hs
+++ Wordle.hs
@@ -10,6 +10,7 @@ module Wordle
   )
 where
 
+import Data.Char (chr)
 import Data.List (all)
 
 type Answer = String
@@ -17,7 +18,13 @@ type Answer = String
 type Guess = String
 
 data LetterFeedback = Correct | WrongPosition | Wrong
-  deriving (Eq, Show, Ord)
+  deriving (Eq, Ord)
+
+instance Show LetterFeedback where
+  show c = case c of
+    Correct -> [chr 129001]
+    WrongPosition -> [chr 129000]
+    Wrong -> [chr 128997]
 
 type GuessFeedback = [LetterFeedback]
 
@@ -35,5 +42,39 @@ wordLength = 5
 hasWon :: GuessFeedback -> Bool
 hasWon = all (== Correct)
 
-getFeedback :: Answer -> Guess -> GuessFeedback
-getFeedback _ _ = replicate 5 Correct
+getFeedback :: Guess -> Answer -> GuessFeedback
+getFeedback guess answer =
+  let (guess', answer', areCorrect) = filterCorrect guess answer
+      areWrongPosition = getWrongPosition guess' answer'
+   in knitFeedbacks areCorrect areWrongPosition
+
+knitFeedbacks :: [Bool] -> [Bool] -> GuessFeedback
+knitFeedbacks (c : cs) (wp : wps) =
+  case (c, wp) of
+    (True, _) -> Correct : knitFeedbacks cs (wp : wps)
+    (_, True) -> WrongPosition : knitFeedbacks cs wps
+    _ -> Wrong : knitFeedbacks cs wps
+knitFeedbacks cs _ = map (\c -> if c then Correct else Wrong) cs
+
+filterCorrect :: Guess -> Answer -> (String, String, [Bool])
+filterCorrect (g : gs) (a : as)
+  | g == a = (gs', as', True : correct')
+  | otherwise = (g : gs', a : as', False : correct')
+  where
+    (gs', as', correct') = filterCorrect gs as
+filterCorrect _ _ = ("", "", [])
+
+getWrongPosition :: Guess -> Answer -> [Bool]
+getWrongPosition (g : gs) as =
+  let (as', isPresent) = removeIfPresent g as
+      areWrongPosition' = getWrongPosition gs as'
+   in (isPresent : areWrongPosition')
+getWrongPosition _ _ = []
+
+removeIfPresent :: Char -> String -> (String, Bool)
+removeIfPresent c (x : xs)
+  | c == x = (xs, True)
+  | otherwise =
+    let (xs', isPresent) = removeIfPresent c xs
+     in (x : xs', isPresent)
+removeIfPresent _ _ = ([], False)
