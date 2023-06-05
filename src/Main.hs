29fe9ad29460e9bf35e4f80fe98d82f3b9d6967c
ldoig <lachjd@protonmail.com>
2023-06-05 18:27:17 +1000

    abstract Main with Actions

--- src/Main.hs
+++ src/Main.hs
@@ -1,76 +1,76 @@
 module Main where
 
+import Control.Monad (zipWithM_)
 import Data.Char (toLower)
-import Data.List (foldr)
+import Data.Maybe (fromMaybe)
+import Safe (atMay)
 import System.IO (BufferMode (NoBuffering), hSetBuffering, stdout)
-import Wordle
+import Text.Read (readMaybe)
+import Wordle (cheatWordleBot, getRandomAnswer, playWordle, playWordleBot)
 import WordleGuesser (guesser)
+
+data Action = Action {summary :: String, run :: [String] -> [String] -> IO Int}
+
+actions :: [Action]
+actions =
+  [ Action
+      "Watch WordleGuesser guess a random Wordle word"
+      (\guesses answers -> playWordleBot guesses answers guesser Nothing),
+    Action
+      "Watch WordleGuesser guess a Wordle word of your choosing"
+      ( \guesses answers -> do
+          putStr "Enter a word for WordleGuesser to guess: "
+          answer <- getLine
+          playWordleBot guesses answers guesser (Just $ map toLower answer)
+      ),
+    Action
+      "Compete against WordleGuesser to guess a random Wordle word the fastest"
+      ( \guesses answers -> do
+          answer <- getRandomAnswer answers
+          putStrLn "Let's start with the human:"
+          humanScore <- playWordle guesses answers $ Just answer
+          putStrLn "Now it's the machine's turn:"
+          machineScore <- playWordleBot guesses answers guesser $ Just answer
+          if humanScore < machineScore
+            then do
+              putStrLn "Congratulations you win!"
+            else
+              if humanScore == machineScore
+                then do
+                  putStrLn "It's a tie..."
+                else do
+                  putStrLn "The machine wins, unlucky."
+          return 0
+      ),
+    Action
+      "Use WordleGuesser to cheat"
+      ( \guesses answers -> do
+          cheatWordleBot guesses answers guesser
+          return 0
+      ),
+    Action
+      "Play a standard game of Wordle yourself"
+      (\guesses answers -> playWordle guesses answers Nothing)
+  ]
+
+summariseAction :: Action -> Int -> IO ()
+summariseAction (Action summary _) i = putStrLn $ " " ++ show i ++ ") " ++ summary
 
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
   guesses <- lines <$> readFile "wordlists/wordle_guesses.txt"
   answers <- lines <$> readFile "wordlists/wordle_answers.txt"
-  putStrLn "You have 7 options:"
-  putStrLn " 1) Watch WordleGuesser guess a random Wordle word"
-  putStrLn " 2) Watch WordleGuesser guess a Wordle word of your choosing"
-  putStrLn " 3) Compete against WordleGuesser to guess a random Wordle word the fastest!"
-  putStrLn " 4) Use WordleGuesser to cheat, by entering its suggestions and relaying feedback"
-  putStrLn " 5) Watch WordleGuesser guess 10 random Wordle words, and see the average score"
-  putStrLn " 6) Test WordleGuesser on all 2315 Wordle words (takes a long time)"
-  putStrLn " 7) Play a standard game of Wordle yourself"
+  putStrLn $ "You have " ++ (show . length) actions ++ " options"
+  zipWithM_ summariseAction actions [0 ..]
   putStr "Enter your chosen number: "
   input <- getLine
-  putStrLn ""
-  case input of
-    "1" -> do
-      playWordleBot guesses answers guesser Nothing
+  case readMaybe input >>= atMay actions of
+    Just (Action _ run) -> do
+      putStrLn ""
+      run guesses answers
       return ()
-    "2" -> do
-      putStr "Enter a word for WordleGuesser to guess: "
-      answer <- getLine
-      playWordleBot guesses answers guesser (Just $ map toLower answer)
-      return ()
-    "3" -> do
-      answer <- getRandomAnswer answers
-      putStrLn "Let's start with the human:"
-      humanScore <- playWordle guesses answers $ Just answer
-      putStrLn "Now it's the machine's turn:"
-      machineScore <- playWordleBot guesses answers guesser $ Just answer
-      if humanScore < machineScore
-        then do
-          putStrLn "Congratulations you win!"
-        else
-          if humanScore == machineScore
-            then do
-              putStrLn "It's a tie..."
-            else do
-              putStrLn "The machine wins, unlucky."
-    "4" -> do
-      cheatWordleBot guesses answers guesser
-    "5" -> do
-      let n = 10
-      let scores = [playWordleBot guesses answers guesser Nothing | _ <- [1 .. n]]
-      total <- foldr sumIO (return 0) scores
-      let avg_score = fromIntegral total / fromIntegral n
-      putStrLn $ "Average score: " ++ show avg_score
-    "6" -> do
-      putStrLn "Please wait..."
-      let scores = testWordleBot guesses answers guesser
-      let avg_score = fromIntegral (sum scores) / fromIntegral (length scores)
-      putStrLn $ "Average score: " ++ show avg_score
-      print scores
-    "7" -> do
-      _ <- playWordle guesses answers Nothing
-      return ()
-    _ -> do
-      putStrLn "Invalid option"
-      main
+    Nothing -> do
+      putStrLn $ "Invalid option, enter a number between 0 and " ++ show (length actions - 1)
   putStrLn "\n"
   main
-
-sumIO :: IO Int -> IO Int -> IO Int
-sumIO a b = do
-  x <- a
-  y <- b
-  return $ x + y
